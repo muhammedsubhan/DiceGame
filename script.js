10146679fbd5cbb0eb59e79f8bae3483d8a9bd3c
muhammedsubhan <muhammedsubhan692@gmail.com>
2022-11-03 16:57:41 +0500

    Resetting Game / Game Completed

--- script.js
+++ script.js
@@ -12,14 +12,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-let currentScore = 0;
-const scores = [0, 0];
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
-diceEl.classList.add('hidden');
+const init = function () {
+  currentScore = 0;
+  scores = [0, 0];
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -73,3 +84,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// New Game
+
+btnNew.addEventListener('click', init);
